ba0ccd055f06bbc2df180447648a469b1b73f649
sergiosvieira <sergiosvieira@localhost.localdomain>
2024-09-15 18:20:42 -0300

    installation

---
+++ siq.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+git clone git@github.com:sergiosvieira/siq.git
+cd siq
+git checkout -b development
+git pull origin development
+cd siq_project
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+sed -i '49,711s/^/#/' main_app/seeders.py 
+sed -i '734,1365s/^/#/' main_app/seeders.py 
+python manage.py seed
+sed -i '49,96s/^#//' main_app/seeders.py
+python manage.py seed
+sed -i '97,135s/^#//' main_app/seeders.py
+python manage.py seed
+sed -i '136,473s/^#//' main_app/seeders.py
+python manage.py seed
+sed -i '474,711s/^#//' main_app/seeders.py
+python manage.py seed
+sed -i '734,853s/^#//' main_app/seeders.py
+python manage.py seed
+sed -i '854,1365s/^#//' main_app/seeders.py
+python manage.py seed
